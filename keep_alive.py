e494e46d0a9ce0f21ed4f65868f4f7e9170471c5
BoxMehdi <m.sml@gmx.at>
2025-07-20 12:23:34 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,9 @@
 # keep_alive.py
-from flask import Flask
 
-app = Flask('')
+from flask import Flask
+import threading
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -9,3 +11,8 @@ def home():
 
 def run():
     app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.daemon = True
+    t.start()
